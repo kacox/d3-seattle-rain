d07e2e2e034956258b1aeb2945d10b80db550920
Kasey Cox <ka.cox@outlook.com>
2017-12-28 22:29:03 -0500

    Initial commit

---
+++ audit.py
@@ -0,0 +1,12 @@
+"""
+Kasey Cox
+December 2017
+
+Python scripts to audit data contents of "Did it rain in Seattle? 
+(1948-2017)" dataset from Kaggle.com 
+(https://www.kaggle.com/rtatman/did-it-rain-in-seattle-19482017).
+
+This dataset was compiled by NOAA and is in the public domain.
+"""
+# IMPORTS
+import pandas as pd
